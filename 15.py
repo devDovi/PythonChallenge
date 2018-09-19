7024cd2a3c27b12a0c851c85c83b2a71055bfd21
Dokyoon Lim <idk255@naver.com>
2018-09-19 22:08:24 +0900

    Added 15.py

---
+++ 15.py
@@ -0,0 +1,8 @@
+# http://www.pythonchallenge.com/pc/return/uzi.html
+
+import calendar
+import datetime
+
+years =  [year for year in range(1016, 1996, 20) if datetime.date(year, 1, 26).isoweekday() == 1]
+print(years)
+
